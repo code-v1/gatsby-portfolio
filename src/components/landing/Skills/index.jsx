80835d2cc52a93bac43f25bc91c1b20fb82d30b2
Eric Tovar <tovareric@gmail.com>
2019-08-15 22:45:10 -0500

    adjusting name

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Container, Button } from 'Common'
-import dev from 'Static/illustrations/skills.svg'
+import dev from 'Static/illustrations/10x.svg'
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles'
 
 export const Skills = () => (
 	<Wrapper id="about">
 		<SkillsWrapper as={Container}>
 			<Thumbnail>
-				<img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
+				<img src={dev} alt="I’m Eric and I’m a Software Developer!" />
 			</Thumbnail>
 			<Details>
 				<h1>Hi There!</h1>
