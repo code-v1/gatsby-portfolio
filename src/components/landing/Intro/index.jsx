8dc2335365f4b8ae3e9c95df965408825181ca27
Eric Tovar <tovareric@gmail.com>
2019-08-15 22:44:43 -0500

    adjusting name

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Header } from 'Theme'
 import { Container, Button } from 'Common'
-import dev from 'Static/illustrations/dev.svg'
+import dev from 'Static/illustrations/tinker-dev.svg'
 import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles'
 
 export const Intro = () => (
@@ -11,13 +11,13 @@ export const Intro = () => (
 		<IntroWrapper as={Container}>
 			<Details>
 				<h1>Hi There!</h1>
-				<h4>I’m Eric Tovar and I’m a Software Developer!</h4>
+				<h4>I’m Eric and I’m a Software Developer!</h4>
 				<Button as={AnchorLink} href="#contact">
 					Hire me
 				</Button>
 			</Details>
 			<Thumbnail>
-				<img src={dev} alt="I’m Eric Tovar and I’m a Software Developer!" />
+				<img src={dev} alt="I’m Eric and I’m a Software Developer!" />
 			</Thumbnail>
 		</IntroWrapper>
 	</Wrapper>
