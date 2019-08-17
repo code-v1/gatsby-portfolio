117ceae407efda340437f2bbf7a846295a19b2e8
Eric Tovar <tovareric@gmail.com>
2019-08-16 21:11:55 -0500

    editing meta data

--- data/config.js
+++ data/config.js
@@ -1,17 +1,14 @@
 module.exports = {
-	defaultTitle: 'John Doe',
-	logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
-	author: 'John Doe',
+	defaultTitle: 'Eric ♥ Software',
+
+	author: 'Eric Tovar',
 	url: 'https://portfolio.smakosh.com',
-	legalName: 'John Doe',
-	defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+	legalName: 'Eric Tovar',
+	defaultDescription: 'I’m eric and i love to write software and tinker with technologie!',
 	socialLinks: {
-		twitter: 'http://www.twitter.com/smakosh',
-		github: 'https://github.com/smakosh',
-		linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
-		instagram: 'https://instagram.com/smakosh19',
-		youtube: 'https://www.youtube.com/user/smakoshthegamer',
-		google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+		twitter: 'http://www.twitter.com/thetovar',
+		github: 'https://github.com/code-v1',
+		linkedin: 'www.linkedin.com/in/erictovar',
 	},
 	googleAnalyticsID: 'UA-88875900-4',
 	themeColor: '#6b63ff',
@@ -31,5 +28,5 @@ module.exports = {
 		phone: 'phone number',
 	},
 	foundingDate: '2018',
-	recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN'
+	recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN',
 }
