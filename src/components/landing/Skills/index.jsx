32b1b0178e0365e93a99716ec0e82244ba063642
Eric Tovar <tovareric@gmail.com>
2019-08-16 21:11:07 -0500

    adding info

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -13,11 +13,16 @@ export const Skills = () => (
 			<Details>
 				<h1>Hi There!</h1>
 				<p>
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry’s standard dummy.
+					As a developer, I strive to create and implement creative and broad
+					solutions to problems facing users in this digital age. I want to
+					bring my collaborative and diverse background to a mission-based
+					environment, looking to impact its user-base and bring technology to
+					all parts of the world. I want to be able to grow and continually
+					improve alongside my passion, while always searching for the best
+					approach to meet user and company success.
 				</p>
 				<Button as={AnchorLink} href="#contact">
-					Hire me
+					Get in touch
 				</Button>
 			</Details>
 		</SkillsWrapper>
